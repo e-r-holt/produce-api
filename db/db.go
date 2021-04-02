1ad7eb287d58b6e9646406b562fed7b60e5f406a
Eric Holt <johndoe@example.com>
2021-04-02 17:48:31 -0500

    define return

--- db/db.go
+++ db/db.go
@@ -1,20 +1,17 @@
 package db
 
-import "fmt"
-
 type Produce struct {
-	Name string
+	Name  string
 	Price float64
 }
 
-func Database() {
-	var db map[string]Produce
+func Database() (db map[string]Produce) {
+	// var db map[string]Produce
 
 	db = make(map[string]Produce)
 	db["A12T-4GH7-QPL9-3N4M"] = Produce{"Lettuce", 3.46}
 	db["E5T6-9UI3-TH15-QR88"] = Produce{"Peach", 2.99}
 	db["YRT6-72AS-K736-L4AR"] = Produce{"Green Pepper", 0.79}
 	db["TQ4C-VV6T-75ZX-1RMR"] = Produce{"Gala Apple", 3.59}
-
-	fmt.Println(db["TQ4C-VV6T-75ZX-1RMR"])
+	return
 }
