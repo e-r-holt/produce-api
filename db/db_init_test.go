724f256c49ea0c64be2474d4814df6962adb321a
Eric Holt <johndoe@example.com>
2021-04-02 18:33:56 -0500

    use each member

--- db/db_init_test.go
+++ db/db_init_test.go
@@ -1,7 +1,19 @@
-package db 
+package db
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
-func TestDbInit() {
-	db := db.Database()
+
+func TestDatabase(t *testing.T) {
+	data := Database()
+
+	for i:=0; i<len(data); i++ {
+		fmt.Println("Code", data[i].Code)
+		fmt.Println("Name", data[i].Name)
+		fmt.Println("Price $", data[i].Price)
+		fmt.Println()
+	}
+	
 }
