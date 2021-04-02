8160c620bbf05ccdc197320e6c3e6a69aeff9984
Eric Holt <johndoe@example.com>
2021-04-02 18:09:37 -0500

    restructure db

--- db/db.go
+++ db/db.go
@@ -2,12 +2,11 @@ package db
 
 type Produce struct {
 	Name  string
+	Code string
 	Price float64
 }
 
 func Database() (db map[string]Produce) {
-	// var db map[string]Produce
-
 	db = make(map[string]Produce)
 	db["A12T-4GH7-QPL9-3N4M"] = Produce{"Lettuce", 3.46}
 	db["E5T6-9UI3-TH15-QR88"] = Produce{"Peach", 2.99}
