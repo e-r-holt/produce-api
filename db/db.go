4e3cfb0da0c86dd548384a5a93a50a2d67f48cb1
Eric Holt <johndoe@example.com>
2021-04-03 10:55:28 -0500

    read and tests

--- db/db.go
+++ db/db.go
@@ -1,13 +1,51 @@
 package db
 
+import "errors"
+
 type Produce struct {
 	Code  string
 	Name  string
 	Price float64
 }
+type ProduceSlice []Produce
 
-func Database() (db []Produce) {
-	db = []Produce{
+//return one record
+//arg == Produce Code
+func (ps ProduceSlice) ReadOne(code string) (*Produce, error) {
+	for _, v := range ps {
+		// fmt.Printf("Index = %d, Value = %s", i, v.Name)
+		if v.Code == code {
+			// fmt.Println()
+			// fmt.Println("Found!")
+			return &v, nil
+		}
+	}
+	return nil, errors.New("can't find the produce code")
+}
+
+//return all records
+func (ps *ProduceSlice) ReadAll() {
+
+}
+
+//create one record
+func (ps *ProduceSlice) CreateOne() {
+
+}
+
+//create many records
+func (ps *ProduceSlice) CreateMany() {
+
+}
+
+//delete one record
+func (ps *ProduceSlice) DeleteOne() {
+
+}
+
+//initialize database for produce API
+func Database() (db ProduceSlice) {
+	db = ProduceSlice{
 		{"A12T-4GH7-QPL9-3N4M", "Lettuce", 3.46},
 		{"E5T6-9UI3-TH15-QR88", "Peach", 2.99},
 		{"YRT6-72AS-K736-L4AR", "Green Pepper", 0.79},
--- db/db_test.go
+++ db/db_test.go
@@ -1,18 +1,40 @@
 package db
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestDatabase(t *testing.T) {
+// func TestDatabase(t *testing.T) {
+// 	data := Database()
+
+// 	for i := 0; i < len(data); i++ {
+// 		fmt.Println("Code", data[i].Code)
+// 		fmt.Println("Name", data[i].Name)
+// 		fmt.Println("Price $", data[i].Price)
+// 		fmt.Println()
+// 	}
+
+// }
+
+func TestGoodRead(t *testing.T) {
 	data := Database()
 
-	for i := 0; i < len(data); i++ {
-		fmt.Println("Code", data[i].Code)
-		fmt.Println("Name", data[i].Name)
-		fmt.Println("Price $", data[i].Price)
-		fmt.Println()
+	pro, err := data.ReadOne("A12T-4GH7-QPL9-3N4M")
+	if err != nil {
+		t.Error("raised error for good read")
+	}
+	if pro.Name != "Lettuce" {
+		t.Error("pulled the wrong produce")
+	}
+
+}
+
+func TestBadRead(t *testing.T) {
+	data := Database()
+
+	_, err := data.ReadOne("foo")
+	if err == nil {
+		t.Error("did not error on bad code")
 	}
 
 }
