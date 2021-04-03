0f9245badc7fda419a6c1ca0165135d3c3e5d0f8
Eric Holt <johndoe@example.com>
2021-04-03 11:43:15 -0500

    delete one, no test

--- db/db.go
+++ db/db.go
@@ -47,6 +47,14 @@ func (ps ProduceSlice) CreateMany(new []Produce) ProduceSlice {
 //if no change, returns given slice
 func (ps ProduceSlice) DeleteOne(code string) ProduceSlice {
 
+	for i, v := range ps {
+		if v.Code == code {
+			ps[i] = ps[len(ps)-1]
+			//no need to put ps[i] at the end, since it will be discarded
+			return ps[:len(ps)-1]
+		}
+	}
+	return ps
 }
 
 //initialize database for produce API
