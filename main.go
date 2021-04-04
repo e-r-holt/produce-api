15aeb0a2c5da40f298399fff896313526575b7e5
Eric Holt <johndoe@example.com>
2021-04-04 13:11:12 -0500

    try body parse

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/e-r-holt/produce-api/db" //"database" operations
+	"github.com/e-r-holt/produce-api/db"
+	store "github.com/e-r-holt/produce-api/db" //"database" operations
 
 	"github.com/gofiber/fiber/v2" //API framework
 )
@@ -28,6 +29,20 @@ func main() {
 		} else { //if no param
 			return c.JSON(db)
 		}
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		c.Accepts("application/json")
+		// Get raw body from POST request:
+		new := new(store.Produce)
+		if err := c.BodyParser(new); err != nil {
+			return err
+		}
+
+		// if db.IsDuplicate(new.Code) != true {
+
+		// }
+		return c.JSON(new) // []byte("user=john")
 	})
 	log.Fatal(app.Listen(":3000"))
 }
