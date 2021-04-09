6775a2ddc7c7b38105f482cdcf521a9e41520ed0
Eric Holt <johndoe@example.com>
2021-04-08 22:57:40 -0500

    one test

--- main_test.go
+++ main_test.go
@@ -2,9 +2,7 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http/httptest"
@@ -52,75 +50,54 @@ func TestGetAll(t *testing.T) {
 	// ################################################################
 }
 
-func TestGetValid(t *testing.T) {
-	app := appSetup()
-
-	//get one good record
-	var expect = []Produce{
-		{
-			Code:  "E5T6-9UI3-TH15-QR88",
-			Name:  "Peach",
-			Price: 2.99,
-		},
-	}
-	url := "/" + expect[0].Code
-	resp, err := app.Test(httptest.NewRequest("GET", url, nil))
-	utils.AssertEqual(t, nil, err, "app.Test")
-
-	if resp.Body != nil {
-		defer resp.Body.Close()
-	}
-
-	body, readErr := ioutil.ReadAll(resp.Body)
-	if readErr != nil {
-		t.Error(readErr)
-	}
-
-	respProduce := []Produce{}
-	jsonErr := json.Unmarshal(body, &respProduce)
-	if jsonErr != nil {
-		log.Printf("error decoding response: %v", jsonErr)
-		if e, ok := err.(*json.SyntaxError); ok {
-			log.Printf("syntax error at byte offset %d", e.Offset)
-		}
-		log.Printf("response: %q", body)
-	}
-	utils.AssertEqual(t, expect, respProduce, "app.Test")
-	utils.AssertEqual(t, 200, resp.StatusCode, "Status code")
-}
-
-func TestGetInalid(t *testing.T) {
-	app := appSetup()
-
-	url := "/asdf"
-	resp, err := app.Test(httptest.NewRequest("GET", url, nil))
-	utils.AssertEqual(t, 404, resp.StatusCode, "Status code")
-	utils.AssertEqual(t, nil, err, "app.Test")
-}
-
-func TestCreateOne(t *testing.T) {
-	app := appSetup()
-
-	url := "/"
-	payload := []Produce{
-		{
-			Code:  "some-ting-real-cool",
-			Name:  "a",
-			Price: 5.46,
-		},
-	}
-	fmt.Println(payload)
-	reqPayload, err := json.Marshal(payload)
-	if err != nil {
-		t.Log(err)
-	}
-	resp, err := app.Test(httptest.NewRequest("POST", url, bytes.NewReader(reqPayload)))
-	utils.AssertEqual(t, 201, resp.StatusCode, "Status code")
-	utils.AssertEqual(t, nil, err, "app.Test")
-}
-
 //
-// func TestCreateMany(t *testing.T) {
+// func TestGetValid(t *testing.T) {
+// 	app := appSetup()
+//
+// 	//get one good record
+// 	var expect = []Produce{
+// 		{
+// 			Code:  "E5T6-9UI3-TH15-QR88",
+// 			Name:  "Peach",
+// 			Price: 2.99,
+// 		},
+// 	}
+// 	url := "/" + expect[0].Code
+// 	resp, err := app.Test(httptest.NewRequest("GET", url, nil))
+// 	utils.AssertEqual(t, nil, err, "app.Test")
+//
+// 	if resp.Body != nil {
+// 		defer resp.Body.Close()
+// 	}
+//
+// 	body, readErr := ioutil.ReadAll(resp.Body)
+// 	if readErr != nil {
+// 		t.Error(readErr)
+// 	}
+//
+// 	respProduce := []Produce{}
+// 	jsonErr := json.Unmarshal(body, &respProduce)
+// 	if jsonErr != nil {
+// 		log.Printf("error decoding response: %v", jsonErr)
+// 		if e, ok := err.(*json.SyntaxError); ok {
+// 			log.Printf("syntax error at byte offset %d", e.Offset)
+// 		}
+// 		log.Printf("response: %q", body)
+// 	}
+// 	utils.AssertEqual(t, expect, respProduce, "app.Test")
+// 	utils.AssertEqual(t, 200, resp.StatusCode, "Status code")
+// }
+//
+// func TestGetInalid(t *testing.T) {
+// 	app := appSetup()
+//
+// 	url := "/asdf"
+// 	resp, err := app.Test(httptest.NewRequest("GET", url, nil))
+// 	utils.AssertEqual(t, 404, resp.StatusCode, "Status code")
+// 	utils.AssertEqual(t, nil, err, "app.Test")
+// }
+//
+// func TestCreateOne(t *testing.T) {
 // 	app := appSetup()
 //
 // 	url := "/"
@@ -130,43 +107,66 @@ func TestCreateOne(t *testing.T) {
 // 			Name:  "a",
 // 			Price: 5.46,
 // 		},
-// 		{
-// 			Code:  "some-tiag-real-cool",
-// 			Name:  "b",
-// 			Price: 5.46,
-// 		},
-// 		{
-// 			Code:  "aome-ting-real-cool",
-// 			Name:  "c",
-// 			Price: 5.46,
-// 		},
-// 		{
-// 			Code:  "some-ting-real-aool",
-// 			Name:  "d",
-// 			Price: 5.46,
-// 		},
-// 		{
-// 			Code:  "some-ting-real-coo",
-// 			Name:  "e",
-// 			Price: 5.46,
-// 		},
-// 		{
-// 			Code:  "some-tingreal-cool",
-// 			Name:  "f",
-// 			Price: 5.46,
-// 		},
-// 		{
-// 			Code:  "some-ting-realcool",
-// 			Name:  "g",
-// 			Price: 5.46,
-// 		},
-// 		{
-// 			Code:  "some-ting-real-cool",
-// 			Name:  "h",
-// 			Price: 5.46,
-// 		},
 // 	}
-// 	resp, err := app.Test(httptest.NewRequest("GET", url, payload))
+// 	reqPayload, err := json.Marshal(payload)
+// 	fmt.Println(payload)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	body := string(reqPayload)
+// 	resp, err := app.Test(httptest.NewRequest("POST", url, strings.NewReader(body)))
 // 	utils.AssertEqual(t, 201, resp.StatusCode, "Status code")
 // 	utils.AssertEqual(t, nil, err, "app.Test")
 // }
+//
+// //
+// // func TestCreateMany(t *testing.T) {
+// // 	app := appSetup()
+// //
+// // 	url := "/"
+// // 	payload := []Produce{
+// // 		{
+// // 			Code:  "some-ting-real-cool",
+// // 			Name:  "a",
+// // 			Price: 5.46,
+// // 		},
+// // 		{
+// // 			Code:  "some-tiag-real-cool",
+// // 			Name:  "b",
+// // 			Price: 5.46,
+// // 		},
+// // 		{
+// // 			Code:  "aome-ting-real-cool",
+// // 			Name:  "c",
+// // 			Price: 5.46,
+// // 		},
+// // 		{
+// // 			Code:  "some-ting-real-aool",
+// // 			Name:  "d",
+// // 			Price: 5.46,
+// // 		},
+// // 		{
+// // 			Code:  "some-ting-real-coo",
+// // 			Name:  "e",
+// // 			Price: 5.46,
+// // 		},
+// // 		{
+// // 			Code:  "some-tingreal-cool",
+// // 			Name:  "f",
+// // 			Price: 5.46,
+// // 		},
+// // 		{
+// // 			Code:  "some-ting-realcool",
+// // 			Name:  "g",
+// // 			Price: 5.46,
+// // 		},
+// // 		{
+// // 			Code:  "some-ting-real-cool",
+// // 			Name:  "h",
+// // 			Price: 5.46,
+// // 		},
+// // 	}
+// // 	resp, err := app.Test(httptest.NewRequest("GET", url, payload))
+// // 	utils.AssertEqual(t, 201, resp.StatusCode, "Status code")
+// // 	utils.AssertEqual(t, nil, err, "app.Test")
+// // }
