133c0faa44ecf25b20b94a12cc67c7c73854dd04
Eric Holt <johndoe@example.com>
2021-04-09 07:49:59 -0500

    go fmt

--- main.go
+++ main.go
@@ -76,6 +76,50 @@ func appSetup() *fiber.App {
 		}
 	})
 
+	//expect list of json objects to add to DB
+	app.Post("/", func(c *fiber.Ctx) error {
+		res := make(chan ProduceSlice)
+		err := make(chan string)
+		c.Accepts("application/json")
+
+		// Get raw body from POST request:
+		new := new(ProduceSlice)
+		if errorStr := c.BodyParser(new); errorStr != nil {
+			fmt.Println(errorStr)
+			return errorStr
+		} else {
+			fmt.Println("Payload: ")
+			fmt.Println(new)
+			// dupe checking
+			var dupes ProduceSlice
+			for _, v := range *new {
+				go db.ReadOne(v.Code, res, err)
+			}
+			for range *new {
+				select {
+				case dupRec := <-res:
+					dupes = append(dupes, dupRec...)
+					// fmt.Println("found a dupe")
+				case <-err: //err means not dupe
+					// fmt.Println("Not a dupe")
+				}
+			}
+
+			if len(dupes) == 0 { //if no dupes, go create
+				for _, v := range *new {
+					go func(v Produce) {
+						db = append(db, v)
+					}(v)
+				}
+
+				c.JSON(new)
+				return c.SendStatus(201)
+			} else {
+				c.JSON(dupes)
+				return c.SendStatus(409)
+			}
+		}
+	})
 	return app
 
 }
